8b9a440c322db39e032971c95e694702d0efcee2
RuDas-KR <jjm1411@naver.com>
2015-03-24 20:20:41 +0900

    Adjust Code

--- LED_Shift/LED_Shift.c
+++ LED_Shift/LED_Shift.c
@@ -2,19 +2,19 @@
 
 int main() {
 	
-	DDRA = 0x81;	//Port A의 끝부분만 사용(Switch)
-	DDRB = 0xFF;	//Port B의 모든부분 사용(LED)
+	DDRA = 0x81;
+	DDRB = 0xFF;
 
-	PORTB = 0x80;	//10000000 (LED)
+	PORTB = 0x80;
 
 	while(1) {
 		if(PINA & 0x80) {
-			if(PORTB = 0x01)
+			if(PORTB != 0x80)
 				PORTB = (1<<PORTB);
 		}
 
 		if(PINA & 0x01) {
-			if(PORTB = 0x80)
+			if(PORTB != 0x01)
 				PORTB = (1>>PORTB);
 		}
 	}
